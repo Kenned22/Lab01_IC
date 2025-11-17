f913e8ae6bc0318daa8d5080059f09836c8a8cf6
Lilly Kennedy <kenned22@pdx.edu>
2025-11-17 09:38:54 -0800

    loading libraries and commenting descriptives

--- Lab02_DataManipVis/Data Manipulation Lab.R
+++
@@ -1,9 +0,0 @@
-#Load relevant libraries - update as needed if there are functions you need that are not in existing libraries
-library(tidyverse)
-library(arrow)
-
-
-#Load and view data - every person make sure you have the data uploaded yourself and added to git ignore
-bids <- read_parquet("C:/Users/LillyKennedy/OneDrive - Portland Psychotherapy/Documents/Lab01_IC/data/bids_data_vDTR.parquet")
-glimpse(bids)
-
